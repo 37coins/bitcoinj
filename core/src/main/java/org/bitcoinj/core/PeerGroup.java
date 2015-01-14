2a2f33f5faf30105c71a44dee87ab23783184607
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2015-01-14 12:09:43 -0300

    Add/Remove wallets to the connected peers as they are added/removed to/from the PeerGroup

--- core/src/main/java/org/bitcoinj/core/PeerGroup.java
+++ core/src/main/java/org/bitcoinj/core/PeerGroup.java
@@ -864,6 +864,9 @@ public class PeerGroup extends AbstractExecutionThreadService implements Transac
             wallet.setTransactionBroadcaster(this);
             wallet.addEventListener(walletEventListener, Threading.SAME_THREAD);
             addPeerFilterProvider(wallet);
+            for (Peer peer : peers) {
+                peer.addWallet(wallet);
+            }
         } finally {
             lock.unlock();
         }
@@ -902,6 +905,9 @@ public class PeerGroup extends AbstractExecutionThreadService implements Transac
         peerFilterProviders.remove(wallet);
         wallet.removeEventListener(walletEventListener);
         wallet.setTransactionBroadcaster(null);
+        for (Peer peer : peers) {
+            peer.removeWallet(wallet);
+        }        
     }
 
     public static enum FilterRecalculateMode {
